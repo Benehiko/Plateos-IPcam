d9e8696ec3e07ee9f8024bb73089e71980a16d5c
Alano Terblanche <alanopi.314@gmail.com>
2018-08-27 03:32:42 +0200

    Added gpu support on display

--- cvShapeHandler/imagedisplay.py
+++ cvShapeHandler/imagedisplay.py
@@ -1,14 +1,15 @@
 import cv2
 import numpy as np
 
+from cvShapeHandler.gpuhandler import GPUHandler
+
+
 class ImageDisplay:
 
     @staticmethod
     def display(img, window_name, size=(640, 480)):
         cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-        cv2.imshow(window_name, cv2.resize(img, size))
-        #cv2.waitKey(0)
-        #cv2.destroyAllWindows()
+        cv2.imshow(window_name, GPUHandler.toUmat(cv2.resize(img, size)))
 
     @staticmethod
     def display_array(arr, window_name):
