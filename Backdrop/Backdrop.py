0f82f97dbea92fa5790940de814314406389a050
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2018-08-29 21:51:26 +0200

    Future changes coming
    Update to cropping
    Changed no more inverse for tess
    Future feature (highlight each character)

--- Backdrop/Backdrop.py
+++ Backdrop/Backdrop.py
@@ -9,7 +9,6 @@ from Caching.CacheHandler import CacheHandler
 from Network.PortScanner import PortScanner
 from Network.requestor import Request
 from camera.Camera import Camera
-from cvShapeHandler.imagedisplay import ImageDisplay
 from numberplate.Numberplate import Numberplate
 from tess.tesseract import Tess
 
