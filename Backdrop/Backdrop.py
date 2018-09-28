0752535ce54773348ba4508871acd46cfdf13d61
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2018-09-28 08:07:44 +0200

    Cleanup Code

--- Backdrop/Backdrop.py
+++ Backdrop/Backdrop.py
@@ -1,17 +1,7 @@
-import asyncio
-import datetime
-import pathlib
-import sys
-from os import listdir
-from os.path import isfile, join
 from threading import Thread
 
 from Backdrop.BackdropHandler import BackdropHandler
-from Caching.CacheHandler import CacheHandler
 from Network.PortScanner import PortScanner
-from Network.requestor import Request
-from camera.Camera import Camera
-from numberplate.Numberplate import Numberplate
 from tess.tesseract import Tess
 
 
@@ -19,105 +9,9 @@ from tess.tesseract import Tess
 class Backdrop:
 
     def __init__(self, args, iprange, url):
-        self.iprange = iprange
-        self.camera = []
-        self.tess = Tess(backdrop=self)
-        self.active = set()
-        self.scanner = PortScanner()
-        self.username, self.password = args
-        self.cached = []
-        self.url = url
-        self.last_upload = None
-        self.backdrophandler = BackdropHandler(self, scanner=PortScanner(), iprange=iprange)
+        self.backdrophandler = BackdropHandler(self, scanner=PortScanner(), iprange=iprange, args=args, url=url)
 
     async def scan(self):
-        t = Thread(target=self.backdrophandler.start()).start()
-        t.join()
-
-    def add(self, a):
-        try:
-            tmp = Camera(username=self.username, password=self.password, ip=a, tess=self.tess)
-            p = Thread(target=tmp.start)
-            self.active.add((a, p))
-            p.start()
-        except:
-            pass
-
-    def callback_tess(self, plate):
-        print("Plate:", plate[0], "Province:", plate[1], "Confidence:", plate[2], "Date:", plate[3])
-        self.cached.append(plate)
-        return
-
-    def cache(self):
-        try:
-            if len(self.cached) >= 1:
-                today = datetime.datetime.now().strftime('%Y-%m-%d %H')
-                self.cached = Numberplate.improve(self.cached)
-                if self.cached is not None:
-                    res = CacheHandler.save("cache/", today, self.cached)
-                    if res is not None:
-                        self.upload_dataset(res)
-                self.cached = []
-        except Exception as e:
-            print(e)
-            pass
-
-    def callback_camera(self, ip):
-        print("Removing camera", ip)
-        self.active.discard(ip)
-
-    def check_alive(self):
-        tmp = self.active.copy()
-        for process in tmp:
-            try:
-                if process[1].is_alive() is False:
-                    print("Process died")
-                    self.active.discard(process)
-            except Exception as e:
-                print("Tried to remove process", e)
-
-    def upload_dataset(self, data):
-        try:
-            url = self.url+"db/addplate"
-            Request.post(data, url)
-        except:
-            pass
-
-    # noinspection PyMethodMayBeStatic
-    def cleanup_cache(self):
-        try:
-            files = [f.replace('.npy.gz', '') for f in listdir("cache") if isfile(join("cache", f))]
-            file_last_date = datetime.datetime.strptime(max(files), "%Y-%m-%d %H")
-            now = datetime.datetime.now()
-            diff = now - file_last_date
-            if datetime.timedelta(days=90) < diff:
-                CacheHandler.remove("cache/", file_last_date.strftime("%Y-%m-%d %H"))
-        except Exception as e:
-            print(e)
-            pass
-
-    def offline_check(self):
-        if Request.check_connectivity():
-            try:
-                pathlib.Path("offline").mkdir(parents=False, exist_ok=True)
-                files = [f.replace('.npy.gz', '') for f in listdir("offline") if isfile(join("offline", f))]
-                if len(files) > 0:
-                    for x in files:
-                        tmp = CacheHandler.load("offline/", x).tolist()
-                        Request.post(tmp, self.url)
-                        CacheHandler.remove("offline/", x)
-            except Exception as e:
-                print(e)
-                pass
-
-    def ping_location(self):
-        try:
-            if Request.check_connectivity():
-                url = self.url+"db/addlocation"
-                Request.ping_location(url)
-        except:
-            pass
-
-
-    def getActive(self):
-        return self.active
+        while True:
+            t = Thread(target=self.backdrophandler.start()).start()
+            t.join()
--- Backdrop/BackdropHandler.py
+++ Backdrop/BackdropHandler.py
@@ -1,25 +1,124 @@
+import datetime
+import pathlib
+from os import listdir
+from os.path import isfile, join
+from threading import Thread
 from time import sleep
+
+from Caching.CacheHandler import CacheHandler
+from Network.requestor import Request
+from camera.Camera import Camera
+from numberplate.Numberplate import Numberplate
+from tess.tesseract import Tess
 
 
 class BackdropHandler:
 
-    def __init__(self, backdrop, scanner, iprange):
+    def __init__(self, backdrop, scanner, iprange, args, url):
         self.backdrop = backdrop
         self.scanner = scanner
         self.iprange = iprange
+        self.username, self.password = args
+        self.tess = Tess(backdrop=self)
+        self.cached = []
+        self.active = set()
+        self.url = url
 
     def start(self):
         while True:
-            active = self.backdrop.getActive()
+            active = self.active
             print("Current Active cameras", active)
             tmp = self.scanner.scan(self.iprange)
             active_ip = [x[0] for x in active]
             for x in tmp:
                 if x not in active_ip:
-                    self.backdrop.add(x)
+                    self.add(x)
             sleep(60)
-            self.backdrop.check_alive()
-            self.backdrop.cleanup_cache()
-            self.backdrop.offline_check()
-            self.backdrop.ping_location()
-            self.backdrop.cache()
+            self.check_alive()
+            self.cleanup_cache()
+            self.offline_check()
+            self.ping_location()
+            self.cache()
+
+    def callback_tess(self, plate):
+        print("Plate:", plate[0], "Province:", plate[1], "Confidence:", plate[2], "Date:", plate[3])
+        self.cached.append(plate)
+        return
+
+    def add(self, a):
+        try:
+            tmp = Camera(username=self.username, password=self.password, ip=a, tess=self.tess)
+            p = Thread(target=tmp.start)
+            self.active.add((a, p))
+            p.start()
+        except:
+            pass
+
+    def cache(self):
+        cached = self.cached
+        try:
+            if len(cached) >= 1:
+                today = datetime.datetime.now().strftime('%Y-%m-%d %H')
+                cached = Numberplate.improve(cached)
+                if cached is not None:
+                    res = CacheHandler.save("cache/", today, cached)
+                    if res is not None:
+                        self.upload_dataset(res)
+                self.cached = []
+        except Exception as e:
+            print(e)
+            pass
+
+    def check_alive(self):
+        tmp = self.active.copy()
+        for process in tmp:
+            try:
+                if process[1].is_alive() is False:
+                    print("Process died")
+                    self.active.discard(process)
+            except Exception as e:
+                print("Tried to remove process", e)
+
+    def upload_dataset(self, data):
+        try:
+            url = self.url
+            url = url+"db/addplate"
+            Request.post(data, url)
+        except:
+            pass
+
+    # noinspection PyMethodMayBeStatic
+    def cleanup_cache(self):
+        try:
+            files = [f.replace('.npy.gz', '') for f in listdir("cache") if isfile(join("cache", f))]
+            file_last_date = datetime.datetime.strptime(max(files), "%Y-%m-%d %H")
+            now = datetime.datetime.now()
+            diff = now - file_last_date
+            if datetime.timedelta(days=90) < diff:
+                CacheHandler.remove("cache/", file_last_date.strftime("%Y-%m-%d %H"))
+        except Exception as e:
+            print(e)
+            pass
+
+    def offline_check(self):
+        if Request.check_connectivity():
+            try:
+                pathlib.Path("offline").mkdir(parents=False, exist_ok=True)
+                files = [f.replace('.npy.gz', '') for f in listdir("offline") if isfile(join("offline", f))]
+                if len(files) > 0:
+                    for x in files:
+                        tmp = CacheHandler.load("offline/", x).tolist()
+                        Request.post(tmp, self.url)
+                        CacheHandler.remove("offline/", x)
+            except Exception as e:
+                print(e)
+                pass
+
+    def ping_location(self):
+        try:
+            if Request.check_connectivity():
+                url = self.url
+                url = url+"db/addlocation"
+                Request.ping_location(url)
+        except:
+            pass
