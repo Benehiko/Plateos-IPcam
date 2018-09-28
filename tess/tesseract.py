4aafbb1f24e7cd21d8f17665cb5c8cdd2df278a6
Alano Terblanche <alanoterblanche@gmail.com>
2018-09-28 07:15:30 +0200

    Changed the way tesseract works

--- tess/tesseract.py
+++ tess/tesseract.py
@@ -11,7 +11,7 @@ from numberplate.Numberplate import Numberplate
 
 class Tess:
 
-    def __init__(self, backdrop):
+    def __init__(self):#backdrop):
         # noinspection PyArgumentList,PyArgumentList
         self.t = PyTessBaseAPI(psm=PSM.CIRCLE_WORD, oem=OEM.TESSERACT_LSTM_COMBINED)
         self.t.SetVariable("load_system_dawg", "false")
@@ -24,7 +24,7 @@ class Tess:
         self.t.SetVariable("tessedit_create_hocr", "0")
         self.t.SetVariable("textord_force_make_prop_words", "false")
         self.t.SetVariable("tessedit_char_whitelist", "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-        self.backdrop = backdrop
+        # self.backdrop = backdrop
 
     def runner(self, image):
         if image is not None:
@@ -37,47 +37,54 @@ class Tess:
                     image = ImageUtil.compress(image, max_w=200)
                     now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                     plate = (text, plate_type, confidence, now, image)
-                    self.backdrop.callback_tess(plate)
+                    # self.backdrop.callback_tess(plate)
+                    return plate
+        return None
 
     def multi(self, images):
         if images is not None:
             if len(images) > 0:
                 pool = mp.Pool(processes=len(images))
-                [pool.apply_async(self.runner(i)) for i in images]
-                pool.close()
-                pool.join()
+                out = [pool.apply_async(self.runner(i)) for i in images]
+                if out is not None:
+                    result = [o.get() for o in out]
+                    result = [r for r in result if r is not None]
+                    pool.close()
+                    pool.join()
+                    return result
+        return None
 
-    def process(self, image):
-        if image is not None:
-            tmp = Image.fromarray(np.uint8(image))
-            self.t.SetImage(tmp)
-            text = Numberplate.sanitise(self.t.GetUTF8Text())
-            plate_type, confidence = Numberplate.validate(text, use_provinces=True)
-            if plate_type is not None and confidence > 0:
-                image = ImageUtil.compress(image, max_w=200)
-                now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                self.backdrop.callback_tess((text, plate_type, confidence, now, image))
-
-    def process_with_roi(self, image, rectangles):
-        chars = []
-
-        if image is not None:
-            tmp = Image.fromarray(np.uint8(image))
-            self.t.SetImage(tmp)
-            for roi in rectangles:
-                ((x1, y1), (x2, y2), _) = roi
-                x1 = int(x1)
-                y1 = int(y1)
-                x2 = int(x2)
-                y2 = int(y2)
-                self.t.SetRectangle(x1, y1, x2, y2)
-                chars.append(Numberplate.sanitise(self.t.GetUTF8Text()))
-            print(chars)
-
-            text = ''.join(chars)
-            result, confidence = Numberplate.validate(text, use_provinces=True)
-            if result is not None:
-                self.backdrop.callback_tess((text, image))
-
-    def download(self):
-        pass
+    # def process(self, image):
+    #     if image is not None:
+    #         tmp = Image.fromarray(np.uint8(image))
+    #         self.t.SetImage(tmp)
+    #         text = Numberplate.sanitise(self.t.GetUTF8Text())
+    #         plate_type, confidence = Numberplate.validate(text, use_provinces=True)
+    #         if plate_type is not None and confidence > 0:
+    #             image = ImageUtil.compress(image, max_w=200)
+    #             now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    #             self.backdrop.callback_tess((text, plate_type, confidence, now, image))
+    #
+    # def process_with_roi(self, image, rectangles):
+    #     chars = []
+    #
+    #     if image is not None:
+    #         tmp = Image.fromarray(np.uint8(image))
+    #         self.t.SetImage(tmp)
+    #         for roi in rectangles:
+    #             ((x1, y1), (x2, y2), _) = roi
+    #             x1 = int(x1)
+    #             y1 = int(y1)
+    #             x2 = int(x2)
+    #             y2 = int(y2)
+    #             self.t.SetRectangle(x1, y1, x2, y2)
+    #             chars.append(Numberplate.sanitise(self.t.GetUTF8Text()))
+    #         print(chars)
+    #
+    #         text = ''.join(chars)
+    #         result, confidence = Numberplate.validate(text, use_provinces=True)
+    #         if result is not None:
+    #             return (text, image) # self.backdrop.callback_tess((text, image))
+    #
+    # def download(self):
+    #     pass
