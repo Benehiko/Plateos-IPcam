de3dc232a9952f72f96de7a89c38dd071b53eba5
Alano Terblanche <18033717+Benehiko@users.noreply.github.com>
2018-12-29 23:21:53 +0200

    Bug fixes
    Fixed spam uploads to more uniform upload and confidence build.
    Fixed some problems with slicing error inside Numberplatehandler.
    Added some extra error checking and data checking to prevent errors.

--- Handlers/ThreadHandler.py
+++ Handlers/ThreadHandler.py
@@ -8,11 +8,14 @@ class ThreadWithReturnValue(Thread):
         self._return = None
 
     def run(self):
-        # print(type(self._target))
         if self._target is not None:
             self._return = self._target(*self._args,
                                         **self._kwargs)
 
     def join(self, *args):
+        """
+
+        :rtype: Thread method return
+        """
         Thread.join(self, *args)
         return self._return
